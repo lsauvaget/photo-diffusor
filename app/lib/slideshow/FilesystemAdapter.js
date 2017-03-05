89544b6a43220a0021dbfc0ec02e09edd64e1319
Léo Sauvaget <yatmak@gmail.com>
2017-03-05 22:33:34 +0100

    add full size

--- app/lib/slideshow/FilesystemAdapter.js
+++ app/lib/slideshow/FilesystemAdapter.js
@@ -6,34 +6,51 @@ Promise.promisifyAll(fs);
 
 module.exports = (options) => {
     const thumbnailFile = `${options.imagesPath}/thumbnail`;
+    const fullFile = `${options.imagesPath}/full`;
 
     function getImages() {
-        return Promise.resolve()
-            .then(() => fs.readdirAsync(options.imagesPath))
+        return fs.readdirAsync(options.imagesPath)
             .then(files => {
                 return files.filter(e => /\.jpg$/.test(e));
             })
+            .then(images => {
+                try{
+                    fs.rmrfSync(thumbnailFile);
+                    fs.rmrfSync(fullFile);
+                }catch(err){}
+                fs.mkdirSync(thumbnailFile);
+                fs.mkdirSync(fullFile);
+                return images;
+            })
         .then(images => {
-            try{
-                fs.rmrfSync(thumbnailFile);
-            }catch(err){}
-            fs.mkdirSync(thumbnailFile);
-            const promises = images.map((image) => {
+            const thumbnailPromises = Promise.all(images.map((image) => {
                 return new Promise((resolve, reject) => {
                     sharp(`${options.imagesPath}/${image}`)
                         .resize(600, 400)
                         .crop(sharp.strategy.attention)
+                        .jpeg({progressive: true})
                         .toFile(`${thumbnailFile}/${image}`, err => {
                             err ? reject(`${err} : ${options.imagesPath}/${image}`) : resolve(image);
                         });
                 });
-            });
-            return Promise.all(promises);
+            }));
+            const fullPromise = Promise.all(images.map((image) => {
+                return new Promise((resolve, reject) => {
+                    sharp(`${options.imagesPath}/${image}`)
+                    .resize(1920, 1920)
+                    .max()
+                    .jpeg({progressive: true})
+                    .toFile(`${fullFile}/${image}`, err => {
+                            err ? reject(`${err} : ${options.imagesPath}/${image}`) : resolve(image);
+                    });
+                });
+            }));
+            return Promise.all([thumbnailPromises, fullPromise]);
         })
-        .then(images => {
-            return images.map(image => ({
+        .then(([fulls, thumbnails]) => {
+            return fulls.map(image => ({
                 name: image,
-                fullSize: `${options.publicImagePath}/${encodeURI(image)}`,
+                fullSize: `${options.publicImagePath}/full/${encodeURI(image)}`,
                 thumbnail:`${options.publicImagePath}/thumbnail/${encodeURI(image)}` 
             }))
         })
